25f31f983d6441004d7541ef44c8d2713b88f6e9
Xein <72787884+ynks@users.noreply.github.com>
2023-04-12 15:53:21 +0200

    Player rotates towards mouse

--- Assets/Scripts/CharacterActions.cs
+++ Assets/Scripts/CharacterActions.cs
@@ -6,7 +6,9 @@ public class Character : MonoBehaviour
 {
 
     public float speed;
+    public Camera camera;
     private Vector2 direction;
+    private Vector2 mousePos;
     private Rigidbody2D rb;
 
     private void Awake()
@@ -19,10 +21,17 @@ public class Character : MonoBehaviour
     void FixedUpdate()
     {
         rb.velocity = direction * speed;
+        Vector2 lookDir = mousePos - rb.position;
+        rb.rotation = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
     }
 
     public void Move(InputAction.CallbackContext input)
     {
         direction = input.ReadValue<Vector2>().normalized;
+    }
+
+    public void Mouse(InputAction.CallbackContext input)
+    {
+        mousePos = camera.ScreenToWorldPoint(input.ReadValue<Vector2>());
     }
 }
